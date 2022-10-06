dd54b077a6b9548006924c46e2448a5a7950d039
nurilloh2004 <nurillohpython@gmail.com>
2022-10-06 15:59:43 +0500

    jazzmin and all forms

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,6 +68,16 @@ TEMPLATES = [
         },
     },
 ]
+
+
+
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "darkly",
+}
+
+
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
